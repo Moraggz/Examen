6ad7ed620bdef483bc1abb9fe0561046fa0d02e2
Luis Mora <148720790+Moraggz@users.noreply.github.com>
2024-04-26 23:10:15 -0600

    Update Utilities.java

--- ExamenFinal/Utilities.java
+++ ExamenFinal/Utilities.java
@@ -2,8 +2,7 @@ package examenFinal;
 import java.awt.*;
 
 class Utilities {
-    public static Point calculateGridPosition(Point mousePoint) {
-        int cellSize = 10;
+    public static Point calculateGridPosition(Point mousePoint, int cellSize) {
         int x = mousePoint.x / cellSize;
         int y = mousePoint.y / cellSize;
         return new Point(x, y);
