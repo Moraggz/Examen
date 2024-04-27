f2e9f8e8f21a167882a12ca96522a5987ffc1d86
Luis Mora <148720790+Moraggz@users.noreply.github.com>
2024-04-26 21:31:26 -0600

    Update MainWindow.java

--- ExamenFinal/MainWindow.java
+++ ExamenFinal/MainWindow.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
-
 public class MainWindow extends JFrame implements Serializable {
     private static final long serialVersionUID = 1L;
     private CanvasPanel canvasPanel;
